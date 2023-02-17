6a58879c30f6e6eb36750f4eb9aecf51257ce538
Douglas Mariano <doug_pinda@hotmail.com>
2023-02-17 14:27:24 -0300

    voltando ao commit de modificacoes do Douglas

--- app.js
+++ app.js
@@ -2,4 +2,3 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
 // Mais um console log de testes
 console.log("O Douglas Fez um comite surpresa")
-console.log("Enviando um console de teste")
