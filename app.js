6027b4f0e2795af9542f8a1f8a19dd4096709452
Douglas Mariano <doug_pinda@hotmail.com>
2023-02-23 19:53:05 -0300

    Testando comando

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
 // Mais um console log de testes
-console.log("O Douglas Fez um comite surpresa")
+console.log("Testando comandos")
